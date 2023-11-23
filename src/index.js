b06d20e88379a39587960827f755e267226a1fa2
AgnieszkaPyzik <agnieszka.piescik@gmail.com>
2023-11-23 19:57:30 +0100

    package.json commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+// import './styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import SlimSelect from 'slim-select';
+// import 'slim-select/dist/slimselect.css';
+import axios from "axios";
